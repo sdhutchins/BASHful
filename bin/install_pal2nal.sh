956ea39fdeca034e10c18c1e00ed2f6c0c442f5b
sdhutchins <sdhutchins@outlook.com>
2019-05-13 18:22:22 -0500

    Added PWD to pal2nal.sh script.

--- bin/install_pal2nal.sh
+++ bin/install_pal2nal.sh
@@ -3,6 +3,7 @@
 # [Shaurita Hutchins] Install PAL2NAL
 #          This script installs PAL2NAL for 64-bit linux distributions.
 
+CWD="$(pwd)"
 # Change to install directory
 cd /usr/local/apps
 
@@ -10,3 +11,9 @@ cd /usr/local/apps
 # PAL2NAL is a perl script so there is no compilation needed
 wget http://www.bork.embl.de/pal2nal/distribution/pal2nal.v14.tar.gz
 tar xzf pal2nal.v14.tar.gz
+rm pal2nal.v14.tar.gz
+cd $CWD
+
+# Go back to starting directory
+PWD="$(pwd)"
+echo "PAL2NAL is currently installed in $PWD"
